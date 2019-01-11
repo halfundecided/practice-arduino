00118dad35fe4c73001e0ed8e21ef7f6e77c20f5
Mijeong Ban <mban1@stevens.edu>
2019-01-10 19:52:04 -0500

    piezo speaker

---
+++ RGB/RGB.ino
@@ -0,0 +1,14 @@
+#define RED 11
+#define GREEN 10
+#define BLUE 9
+
+void setup() {
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+  analogWrite(RED,random(255));
+  analogWrite(GREEN, random(255));
+  analogWrite(BLUE, random(255));
+  delay(1000);
+}
